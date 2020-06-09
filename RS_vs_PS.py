7452ef30ab1673bc84c756c1a3287c8e55e03db8
pkratz22 <51302875+pkratz22@users.noreply.github.com>
2020-06-09 00:49:46 -0400

    Improved so DF has RS and PS

--- RS_vs_PS.py
+++ RS_vs_PS.py
@@ -3,6 +3,7 @@
 import re
 import requests
 from bs4 import BeautifulSoup, SoupStrainer, Comment
+import numpy as np
 import pandas as pd
 
 # get URL for player
@@ -21,66 +22,39 @@ playerPage = requests.get(playerURL).text
 tables = SoupStrainer('table')
 soup = BeautifulSoup(re.sub('<!--|-->','', playerPage), 'lxml', parse_only=tables)
 
-# RSperGame
+# create column headers
+
+perGameColumns = ['Season', 'Age', 'Tm', 'Lg', 'Pos', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'eFG%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', 'RS or PS']
+perMinColumns = ['Season', 'Age', 'Tm', 'Lg', 'Pos', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS']
+perPossColumns = ['Season', 'Age', 'Tm', 'Lg', 'Pos', 'G', 'GS', 'MP', 'FG', 'FGA', 'FG%', '3P', '3PA', '3P%', '2P', '2PA', '2P%', 'FT', 'FTA', 'FT%', 'ORB', 'DRB', 'TRB', 'AST', 'STL', 'BLK', 'TOV', 'PF', 'PTS', '', 'ORtg', 'DRtg']
+advancedColumns = ['Season', 'Age', 'Tm', 'Pos', 'G', 'MP', 'PER', 'TS%', '3PAr', 'FTr', 'ORB%', 'DRB%', 'TRB%', 'AST%', 'STL%', 'BLK%', 'TOV', 'USG%', '', 'OWS', 'DWS', 'WS', 'WS/48', '', 'OBPM', 'DBPM', 'BPM', 'VORP']
+
+# per Game
+
 RSperGame = soup.find(id='per_game')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in RSperGame.find_all("tr")]
-RSperGamedf = pd.DataFrame(tab_data)
-RSperGamedf.columns = RSperGamedf.iloc[0,:]
-RSperGamedf.drop(index = 0, inplace = True)
-print(RSperGamedf)
-
-# RSperMin
-RSperMin = soup.find(id='per_minute')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in RSperMin.find_all("tr")]
-RSperMindf = pd.DataFrame(tab_data)
-RSperMindf.columns = RSperMindf.iloc[0,:]
-RSperMindf.drop(index = 0, inplace = True)
-print(RSperMindf)
-
-# RSperPoss
-RSperPoss = soup.find(id='per_poss')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in RSperPoss.find_all("tr")]
-RSperPossdf = pd.DataFrame(tab_data)
-RSperPossdf.columns = RSperPossdf.iloc[0,:]
-RSperPossdf.drop(index = 0, inplace = True)
-print(RSperPossdf)
-
-# RSadvanced
-RSadvanced = soup.find(id='advanced')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in RSadvanced.find_all("tr")]
-RSadvanceddf = pd.DataFrame(tab_data)
-RSadvanceddf.columns = RSadvanceddf.iloc[0,:]
-RSadvanceddf.drop(index = 0, inplace = True)
-print(RSadvanceddf)
-
-# PSperGame
 PSperGame = soup.find(id='playoffs_per_game')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in PSperGame.find_all("tr")]
-PSperGamedf = pd.DataFrame(tab_data)
-PSperGamedf.columns = PSperGamedf.iloc[0,:]
-PSperGamedf.drop(index = 0, inplace = True)
-print(PSperGamedf)
+RS = [[cell.text for cell in row.find_all(["th","td"])] for row in RSperGame.find_all("tr")]
+PS = [[cell.text for cell in row.find_all(["th","td"])] for row in PSperGame.find_all("tr")]
+del RS[0]
+del PS[0]
+for year in RS:
+    year.extend(['RS'])
+for year in PS:
+    year.extend(['PS'])
+perGameData = RS+PS
+perGameDF = pd.DataFrame(perGameData, columns = perGameColumns)
 
-# PSperMin
-PSperMin = soup.find(id='playoffs_per_minute')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in PSperMin.find_all("tr")]
-PSperMindf = pd.DataFrame(tab_data)
-PSperMindf.columns = PSperMindf.iloc[0,:]
-PSperMindf.drop(index = 0, inplace = True)
-print(PSperMindf)
+# remove empty rows
+perGameDF.replace('', np.nan, inplace = True)
+perGameDF.dropna(subset=['Season'], inplace=True)
+# sorting
+perGameDF.sort_values(by=['Season', 'Tm'], inplace=True)
 
-# PSperPoss
-PSperPoss = soup.find(id='playoffs_per_poss')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in PSperPoss.find_all("tr")]
-PSperPossdf = pd.DataFrame(tab_data)
-PSperPossdf.columns = PSperPossdf.iloc[0,:]
-PSperPossdf.drop(index = 0, inplace = True)
-print(PSperPossdf)
 
-# PSadvanced
-PSadvanced = soup.find(id='playoffs_advanced')
-tab_data = [[cell.text for cell in row.find_all(["th","td"])] for row in PSadvanced.find_all("tr")]
-PSadvanceddf = pd.DataFrame(tab_data)
-PSadvanceddf.columns = PSadvanceddf.iloc[0,:]
-PSadvanceddf.drop(index = 0, inplace = True)
-print(PSadvanceddf)
+print(perGameDF)
+
+# per Min
+
+# per Poss
+
+# advanced
