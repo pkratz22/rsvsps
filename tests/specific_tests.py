ab653e094e8e085269f3722f81a709f96f74e197
pkratz22 <51302875+pkratz22@users.noreply.github.com>
2020-07-23 15:17:55 -0400

    Added tests

--- tests/specific_tests.py
+++ tests/specific_tests.py
@@ -1,5 +1,7 @@
 from .context import RS_vs_PS
 
+import requests
+
 import unittest
 
 class test_RS_vs_PS(unittest.TestCase):
@@ -8,18 +10,26 @@ class test_RS_vs_PS(unittest.TestCase):
     def test_determine_player_URL(self):
         # normal test
         self.assertEqual(RS_vs_PS.determine_player_URL("petrodr01"), "https://www.basketball-reference.com/players/p/petrodr01.html")
-        
-        # blank player_ID
-        with self.assertRaises(IndexError):
-            RS_vs_PS.determine_player_URL("")
 
         # player_ID with quotes
         self.assertEqual(RS_vs_PS.determine_player_URL("'Hello'"), "https://www.basketball-reference.com/players/'/'Hello'.html")
-  
+
+        # blank player_ID
+        with self.assertRaises(SystemExit):
+            RS_vs_PS.determine_player_URL("")
+
     
     def test_scrape_player_page(self):
+        # normal test
+        self.assertIsNotNone(RS_vs_PS.scrape_player_page("https://www.basketball-reference.com/players/p/petrodr01.html"))
 
-        
+        # player missing some tables
+        self.assertIsNotNone(RS_vs_PS.scrape_player_page("https://www.basketball-reference.com/players/c/cousybo01.html"))
+
+        # player page doesn't exist
+        with self.assertRaises(SystemExit):
+             RS_vs_PS.scrape_player_page("https://www.basketball-reference.com/players/3")
+
 
 if __name__ == "__main__":
     unittest.main()
