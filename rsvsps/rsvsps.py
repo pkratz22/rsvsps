3ee1c0ab9b1102fe583298b3cda0b37e1c1af206
pkratz22 <pkratz22@gmail.com>
2021-05-07 22:36:58 -0400

    Fixed quotes and some minor linting

--- rsvsps/rsvsps.py
+++ rsvsps/rsvsps.py
@@ -11,10 +11,10 @@ import pandas as pd
 
 def determine_player_url(player_id):
     """Determine the Player's Page URL from player's ID"""
-    if player_id == "":
+    if player_id == '':
         raise SystemExit(IndexError)
-    base_url = "https://www.basketball-reference.com/players/"
-    return base_url + "{last_initial}/{ID}.html".format(
+    base_url = 'https://www.basketball-reference.com/players/'
+    return base_url + '{last_initial}/{ID}.html'.format(
         last_initial=player_id[0], ID=player_id)
 
 
@@ -27,33 +27,33 @@ def scrape_player_page(player_url):
         raise SystemExit from err
     player_page = page_request.text
     tables = SoupStrainer(id=[
-        "per_game",
-        "playoffs_per_game",
-        "per_minute",
-        "playoffs_per_minute",
-        "per_poss",
-        "playoffs_per_poss",
-        "advanced",
-        "playoffs_advanced",
+        'per_game',
+        'playoffs_per_game',
+        'per_minute',
+        'playoffs_per_minute',
+        'per_poss',
+        'playoffs_per_poss',
+        'advanced',
+        'playoffs_advanced',
     ])
-    player_page = player_page.replace("<!--", "").replace("-->", "")
+    player_page = player_page.replace('<!--', '').replace('-->', '')
 
-    soup = BeautifulSoup(player_page, "lxml", parse_only=tables)
+    soup = BeautifulSoup(player_page, 'lxml', parse_only=tables)
     return soup
 
 
 def scrape_tables(soup, label, table_type):
     """Scrape the PerGameTables from the Player Page"""
-    playoffs_qualifier = ""
-    if label == "PS":
-        playoffs_qualifier = "playoffs_"
+    playoffs_qualifier = ''
+    if label == 'PS':
+        playoffs_qualifier = 'playoffs_'
     return soup.find(id=playoffs_qualifier + table_type)
 
 
 def scraped_table_to_list(table):
     """Convert the scraped table to a multidimensional list"""
-    return [[cell.text for cell in row.find_all(["th", "td"])]
-            for row in table.find_all("tr")]
+    return [[cell.text for cell in row.find_all(['th', 'td'])]
+            for row in table.find_all('tr')]
 
 
 def scrape_column_headers(player_data_list):
@@ -68,13 +68,13 @@ def remove_column_headers(player_data_list):
 
 def remove_blank_lines(player_data_list):
     """Remove blank lines"""
-    return [year for year in player_data_list if year[0] != ""]
+    return [year for year in player_data_list if year[0] != '']
 
 
 def adjustments_for_did_not_play_seasons(player_data_list):
     """Corrects formatting for seasons with Did Not Play"""
-    list_extender = [""] * (len(player_data_list[0]) - 3)
-    return [[*year, *list_extender] if "Did Not Play" in year[2] else year
+    list_extender = [''] * (len(player_data_list[0]) - 3)
+    return [[*year, *list_extender] if 'Did Not Play' in year[2] else year
             for year in player_data_list]
 
 
@@ -90,12 +90,12 @@ def clean_table(soup, label, table_type):
         return None
     player_data_list = scraped_table_to_list(table)
     column_headers = scrape_column_headers(
-        player_data_list) + ["RSPS"] + ["diff_qualifier"]
+        player_data_list) + ['RSPS'] + ['diff_qualifier']
     player_data_list = remove_column_headers(player_data_list)
     player_data_list = remove_blank_lines(player_data_list)
     player_data_list = adjustments_for_did_not_play_seasons(player_data_list)
     player_data_list = label_rs_or_ps(player_data_list, label)
-    if label == "RS":
+    if label == 'RS':
         player_data_list = [column_headers] + player_data_list
     return player_data_list
 
@@ -112,8 +112,8 @@ def combine_rs_and_ps(regular_season, post_season):
 
 def add_sorting_qualifier(player_data_list):
     """Add an element to each row that can be used to properly sort"""
-    return [[*year, "1" + year[2]] if "season" in year[0] else
-            [*year, "2"] if "Career" in year[0] else [*year, "0" + year[0]]
+    return [[*year, '1' + year[2]] if 'season' in year[0] else
+            [*year, '2'] if 'Career' in year[0] else [*year, '0' + year[0]]
             for year in player_data_list]
 
 
@@ -130,15 +130,15 @@ def remove_sorting_column(player_data_list):
 def add_blank_lines(player_data_list):
     """Add blank lines that will store differences"""
     length_appended = max([len(row) for row in player_data_list])
-    player_data_list = player_data_list + [[""] * length_appended]
+    player_data_list = player_data_list + [[''] * length_appended]
     upper_bound = len(player_data_list) - 1
     row = 0
     while row < upper_bound:
-        if ((player_data_list[row][0] != "")
-                & (player_data_list[row + 1][0] != "")
+        if ((player_data_list[row][0] != '')
+                & (player_data_list[row + 1][0] != '')
                 & (player_data_list[row][-1] != player_data_list[row + 1][-1])):
             player_data_list = player_data_list[:row + 1] + [
-                [""] * length_appended] + player_data_list[row + 1:]
+                [''] * length_appended] + player_data_list[row + 1:]
             upper_bound += 1
         row += 1
     return player_data_list
@@ -146,7 +146,7 @@ def add_blank_lines(player_data_list):
 
 def add_qualifier_col_diff(player_data_list):
     """Add qualifier that will determine which rows to take difference from"""
-    return [[*year, ""] for year in player_data_list]
+    return [[*year, ''] for year in player_data_list]
 
 
 def create_dataframe(player_data_list, column_headers):
@@ -157,72 +157,72 @@ def create_dataframe(player_data_list, column_headers):
 def dataframe_data_types(dataframe, table_type):
     """Creates column headers for dataframe"""
     cols = []
-    if (table_type == "per_game" or table_type == "per_minute"
-            or table_type == "per_poss"):
+    if (table_type == 'per_game' or table_type == 'per_minute'
+            or table_type == 'per_poss'):
         possible_columns = [
-            "G",
-            "GS",
-            "MP",
-            "FG",
-            "FGA",
-            "FG%",
-            "3P",
-            "3PA",
-            "3P%",
-            "2P",
-            "2PA",
-            "2P%",
-            "eFG%",
-            "FT",
-            "FTA",
-            "FT%",
-            "ORB",
-            "DRB",
-            "TRB",
-            "AST",
-            "STL",
-            "BLK",
-            "TOV",
-            "PF",
-            "PTS",
-            "ORtg",
-            "DRtg",
+            'G',
+            'GS',
+            'MP',
+            'FG',
+            'FGA',
+            'FG%',
+            '3P',
+            '3PA',
+            '3P%',
+            '2P',
+            '2PA',
+            '2P%',
+            'eFG%',
+            'FT',
+            'FTA',
+            'FT%',
+            'ORB',
+            'DRB',
+            'TRB',
+            'AST',
+            'STL',
+            'BLK',
+            'TOV',
+            'PF',
+            'PTS',
+            'ORtg',
+            'DRtg',
         ]
         for column in possible_columns:
             if column in dataframe.columns:
                 cols += [column]
 
-    elif table_type == "advanced":
+    elif table_type == 'advanced':
         possible_columns = [
-            "G",
-            "MP",
-            "PER",
-            "TS%",
-            "3PAr",
-            "FTr",
-            "ORB%",
-            "DRB%",
-            "TRB%",
-            "AST%",
-            "STL%",
-            "BLK%",
-            "TOV%",
-            "USG%",
-            "OWS",
-            "DWS",
-            "WS",
-            "WS/48",
-            "OBPM",
-            "DBPM",
-            "BPM",
-            "VORP",
+            'G',
+            'MP',
+            'PER',
+            'TS%',
+            '3PAr',
+            'FTr',
+            'ORB%',
+            'DRB%',
+            'TRB%',
+            'AST%',
+            'STL%',
+            'BLK%',
+            'TOV%',
+            'USG%',
+            'OWS',
+            'DWS',
+            'WS',
+            'WS/48',
+            'OBPM',
+            'DBPM',
+            'BPM',
+            'VORP',
         ]
         for column in possible_columns:
             if column in dataframe.columns:
                 cols += [column]
 
     dataframe[cols] = dataframe[cols].apply(pd.to_numeric,
-                                            errors="coerce",
+                                            errors='coerce',
                                             axis=1)
     return dataframe
 
@@ -231,19 +231,19 @@ def determine_rows_to_fill(dataframe):
     """Determine rows with RS (tot) and PS"""
 
     for row in range(len(dataframe.index)):
-        if dataframe.loc[row, "RSPS"] == "":
-            dataframe.loc[row, "diff_qualifier"] = "Diff"
+        if dataframe.loc[row, 'RSPS'] == '':
+            dataframe.loc[row, 'diff_qualifier'] = 'Diff'
 
     for row in range(len(dataframe.index)):
-        if (dataframe.loc[row, "diff_qualifier"] != "Diff" and (
-                row == 0 or dataframe.loc[row - 1, "diff_qualifier"] == "Diff")):
-            dataframe.loc[row, "diff_qualifier"] = "First"
+        if (dataframe.loc[row, 'diff_qualifier'] != 'Diff' and (
+                row == 0 or dataframe.loc[row - 1, 'diff_qualifier'] == 'Diff')):
+            dataframe.loc[row, 'diff_qualifier'] = 'First'
 
     for row in range(len(dataframe.index)):
-        if (dataframe.loc[row, "diff_qualifier"] != "Diff"
-                and (dataframe.loc[row + 1, "diff_qualifier"] == "Diff"
-                     and dataframe.loc[row, "RSPS"] == "PS")):
-            dataframe.loc[row, "diff_qualifier"] = "Last"
+        if (dataframe.loc[row, 'diff_qualifier'] != 'Diff'
+                and (dataframe.loc[row + 1, 'diff_qualifier'] == 'Diff'
+                     and dataframe.loc[row, 'RSPS'] == 'PS')):
+            dataframe.loc[row, 'diff_qualifier'] = 'Last'
 
     return dataframe
 
@@ -253,47 +253,87 @@ def remove_extra_first_last(dataframe):
     first_count = 0
     last_count = 0
     for row in range(len(dataframe.index)):
-        if dataframe.loc[row, "diff_qualifier"] == "First":
+        if dataframe.loc[row, 'diff_qualifier'] == 'First':
             first_count += 1
-        elif dataframe.loc[row, "diff_qualifier"] == "Last":
+        elif dataframe.loc[row, 'diff_qualifier'] == 'Last':
             last_count += 1
         elif dataframe.loc[
-                row, "diff_qualifier"] == "Diff" and first_count != last_count:
+                row, 'diff_qualifier'] == 'Diff' and first_count != last_count:
             first_count = 0
             last_count = 0
-            dataframe.loc[row, "diff_qualifier"] = ""
+            dataframe.loc[row, 'diff_qualifier'] = ''
             temp_row = row - 1
             while temp_row >= 0 and dataframe.loc[temp_row,
-                                                  "diff_qualifier"] != "Diff":
-                dataframe.loc[temp_row, "diff_qualifier"] = ""
+                                                  'diff_qualifier'] != 'Diff':
+                dataframe.loc[temp_row, 'diff_qualifier'] = ''
                 temp_row -= 1
     return dataframe
 
 
 def get_differences(dataframe):
     """Calculate differences between RS and PS"""
-    diff_columns = [
-        "MP", "FG", "FGA", "FG%", "3P", "3PA", "3P%", "2P", "2PA", "2P%",
-        "eFG%", "FT", "FTA", "FT%", "ORB", "DRB", "TRB", "AST", "STL", "BLK",
-        "TOV", "PF", "PTS", "ORtg", "DRtg", "PER", "TS%", "3PAr", "FTr",
-        "ORB%", "DRB%", "TRB%", "AST%", "STL%", "BLK%", "TOV%", "USG%", "OWS",
-        "DWS", "WS", "WS/48", "OBPM", "DBPM", "BPM", "VORP"
-    ]
+    diff_columns = {
+       '"MP',
+       'FG',
+       'FGA',
+       'FG%',
+       '3P',
+       '3PA',
+       '3P%',
+       '2P',
+       '2PA',
+       '2P%',
+       'eFG%',
+       'FT',
+       'FTA',
+       'FT%',
+       'ORB',
+       'DRB',
+       'TRB',
+       'AST',
+       'STL',
+       'BLK',
+       'TOV',
+       'PF',
+       'PTS',
+       'ORtg',
+       'DRtg',
+       'PER',
+       'TS%',
+       '3PAr',
+       'FTr',
+       'ORB%',
+       'DRB%',
+       'TRB%',
+       'AST%',
+       'STL%',
+       'BLK%',
+       'TOV%',
+       'USG%',
+       'OWS',
+       'DWS',
+       'WS',
+       'WS/48',
+       'OBPM',
+       'DBPM',
+       'BPM',
+       'VORP',
+    }
     first = []
     last = []
     for row in range(len(dataframe.index)):
 
-        if dataframe.loc[row, "diff_qualifier"] == "First":
+        if dataframe.loc[row, 'diff_qualifier'] == 'First':
             for col in diff_columns:
                 if col in dataframe:
                     first.append(dataframe.loc[row, col])
 
-        elif dataframe.loc[row, "diff_qualifier"] == "Last":
+        elif dataframe.loc[row, 'diff_qualifier'] == 'Last':
             for col in diff_columns:
                 if col in dataframe:
                     last.append(dataframe.loc[row, col])
 
-        elif dataframe.loc[row, "diff_qualifier"] == "Diff":
+        elif dataframe.loc[row, 'diff_qualifier'] == 'Diff':
             counter = 0
             for col in diff_columns:
                 if col in dataframe:
@@ -312,8 +352,8 @@ def remove_diff_qualifier_column(dataframe):
 
 def player_single_table_type(player_page, table_type):
     """get specific single table for player"""
-    regular_season = clean_table(player_page, "RS", table_type)
-    post_season = clean_table(player_page, "PS", table_type)
+    regular_season = clean_table(player_page, 'RS', table_type)
+    post_season = clean_table(player_page, 'PS', table_type)
     if (regular_season is None) & (post_season is None):
         return pd.DataFrame()
     combined = combine_rs_and_ps(regular_season, post_season)
@@ -373,4 +413,3 @@ if __name__ == '__main__':
     parser.add_argument('--player', type=str)
     args = parser.parse_args()
     main(args.player)
-    
